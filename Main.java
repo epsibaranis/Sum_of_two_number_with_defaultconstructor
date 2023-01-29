8b93f9aa1c5705b42dffdb8754366dc022ebcb52
epsibaranis <epsibaranis@gmail.com>
2023-01-29 19:07:58 +0530

    Sum of two numbers with default constructor

---
+++ Main.java
@@ -0,0 +1,12 @@
+package S2nos;
+
+public class Main {
+
+	public static void main(String[] args) {
+		S2nos a;
+		a=new S2nos();
+		a.find();
+		a.display();
+	}
+
+}
---
+++ S2nos.java
@@ -0,0 +1,14 @@
+package S2nos;
+
+public class S2nos {
+    private int x,y,s;
+    public S2nos() {
+    	x=5;
+    	y=7;
+    }
+    public void find() {
+    	s=x+y;
+    }
+    public void display() {
+    	System.out.println(s);    }
+}
